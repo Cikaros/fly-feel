13117062aaba1a96672454462e16d2006b8f7476
Cikaros <Cikaros@qq.com>
2022-03-22 18:14:16 +0800

    增加页面标题图标

--- fly-feel-doc/docs/.vuepress/.temp/internal/siteData.js
+++ fly-feel-doc/docs/.vuepress/.temp/internal/siteData.js
@@ -3,6 +3,14 @@ export const siteData = {
   "lang": "zh-CN",
   "title": "fly-feel框架",
   "description": "一个集成了常用功能和业务需求的开发框架。",
-  "head": [],
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/images/favicon.svg"
+      }
+    ]
+  ],
   "locales": {}
 }
