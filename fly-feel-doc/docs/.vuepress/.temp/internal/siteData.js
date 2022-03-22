ac2e19ab38cfe7311271b18be8dab985cb1c9e6f
Cikaros <Cikaros@qq.com>
2022-03-22 18:11:54 +0800

    剔除head头信息

--- fly-feel-doc/docs/.vuepress/.temp/internal/siteData.js
+++ fly-feel-doc/docs/.vuepress/.temp/internal/siteData.js
@@ -3,14 +3,6 @@ export const siteData = {
   "lang": "zh-CN",
   "title": "fly-feel框架",
   "description": "一个集成了常用功能和业务需求的开发框架。",
-  "head": [
-    [
-      "link",
-      {
-        "rel": "icon",
-        "href": "./images/favicon.svg"
-      }
-    ]
-  ],
+  "head": [],
   "locales": {}
 }
